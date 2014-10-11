71a33b889b18769aa975cc2d6f54a7c9287c3487
Xuan Luong <luonghoaixuan@gmail.com>
2014-10-11 10:22:01 +0200

    index-ui: mocked data for noteListController

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,23 @@
 var myControllers = angular.module("myControllers", []);
 myControllers.controller("noteListController", [
 	"$scope", function ($scope) {
-
+		$scope.notes = [ 
+			{
+				title: "note 1",
+				url: "#",
+				content: "This is the first note"
+			},
+			{
+				title: "note 2",
+				url: "#",
+				content: "This is the 2nd note"
+			},
+			{
+				title: "note 3",
+				url: "#",
+				content: "This is the 3rd note"
+			},
+		];
 	}
 ]).controller("noteVisController", [
 	"$scope", function ($scope) {
