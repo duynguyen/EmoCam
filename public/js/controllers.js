1016f8c74fd1f808ddb057503e106e8d24b44045
Xuan Luong <luonghoaixuan@gmail.com>
2014-10-11 10:42:39 +0200

    index-ui: show date on noteVis when a note is clicked

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,6 @@
 var myControllers = angular.module("myControllers", []);
-myControllers.controller("noteListController", [
-	"$scope", function ($scope) {
+myControllers.controller("bodyController", [
+	"$scope", function($scope) {
 		$scope.notes = [ 
 			{
 				title: "note 1",
@@ -19,8 +19,19 @@ myControllers.controller("noteListController", [
 			},
 		];
 	}
-]).controller("noteVisController", [
+]).controller("noteListController", [
 	"$scope", function ($scope) {
-		
+		$scope.showNote = function(note) {
+			$scope.$parent.$broadcast("show-note", note);
+		};
+	}
+]).controller("noteVisController", [
+	"$scope", "$document", function ($scope, $document) {
+		$scope.$on("show-note", function(event, data) {
+			displayNote(data);
+		});
+		function displayNote(note) {
+			$("#noteVis").text(note.content);
+		}
 	}
 ]);
